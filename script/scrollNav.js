8df732b8ca663c571cda85b68c8e6e5ce0f4854f
Gabriel Amaral <gabrielmaral1503@gmail.com>
2024-03-30 13:50:25 -0300

    firstcommit

---
+++ script/scrollNav.js
@@ -0,0 +1,10 @@
+// const nav = $('#nav');
+// $(window).scroll(function() {
+//     if ($(this).scrollTop() > 150) {
+//         nav.addClass("menu-fixo");
+//         $('body').css('padding-top', 70);
+//     } else {
+//         nav.removeClass("menu-fixo");
+//         $('body').css('padding-top', 0);
+//     }
+// });
